0b57b73d12db6ac4b2e1c6a7ad0cef8de7e0ce54
zishan ahmed <67750167+supersayko@users.noreply.github.com>
2023-10-19 00:35:01 +0530

    Task1_NumberGuessGame.java

--- Task1_NumberGuessGame.java
+++ Task1_NumberGuessGame.java
@@ -6,30 +6,50 @@ public class Task1_NumberGuessGame {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Random rand = new Random();
-        int attempt = 0;
+
 //        int minRange = 1;
 //        int maxRange = 100;
         int maxAttempt = 7;
 
+        int score = 0;
 
-        System.out.println("-----------Number Game-----------");
-
-        int correct = rand.nextInt(100)+1;
-        boolean isRight = false;
-        while (attempt < maxAttempt) {
-            System.out.println("Enter a guess");
-            int guess = sc.nextInt();
-            attempt++;
-            if (correct == guess) {
-                System.out.println("You are right");
-                isRight=true;
-                break;
-            } else if (correct < guess) {
-                System.out.println("to low\n");
-            } else {
-                System.out.println("to high\n");
+        boolean playAgain = true;
+        while(playAgain) {
+            System.out.println("-----------Number Game-----------");
+            int target = rand.nextInt(100) + 1;
+            int attempt = 0;
+            boolean isTrue = false;
+            while (attempt < maxAttempt) {
+                System.out.println("Enter a guess");
+                int userGuess = sc.nextInt();
+                attempt++;
+                if (target == userGuess) {
+                    System.out.printf("You are right, You guessed the number in %d attempts.\n", attempt);
+                    score += attempt;
+                    isTrue = true;
+                    break;
+                } else if (target < userGuess) {
+                    System.out.println("to high\n");
+                } else {
+                    System.out.println("to low\n");
+                }
             }
-        }
 
+            System.out.println("Your score is " + score);
+
+            if (!isTrue) {
+                System.out.println("Sorry, you have no attempts left. The Correct answer was " + target);
+            }
+
+            System.out.print("You want to play again? (yes/no): ");
+            String playAgainInput = sc.next().toLowerCase();
+
+            if (!playAgainInput.equals("yes")){
+                playAgain=false;
+            }
+
+        }
+        System.out.println("Thank you for playing! Your final score: " + score);
+        sc.close();
     }
 }
